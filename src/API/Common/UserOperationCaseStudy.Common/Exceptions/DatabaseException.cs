82e3995db271599d65cc24e0cb78903ec1cbdd6b
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:13:57 +0300

    Own service exception classes has been added.

---
+++ src/API/Common/UserOperationCaseStudy.Common/Exceptions/DatabaseException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperationCaseStudy.Common.Exceptions
+{
+    public class DatabaseException: Exception
+    {
+        public int StatusCode { get; set; }
+        public DatabaseException(string message) : base(message)
+        {
+        }
+
+        public DatabaseException(string message, int statusCode) : this(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        public DatabaseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
