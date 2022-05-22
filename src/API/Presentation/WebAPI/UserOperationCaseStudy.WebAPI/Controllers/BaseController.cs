79d5d28d1a1896d33056f3ea43966f417df2b620
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:13:29 +0300

    UserController has been added in API.

---
+++ src/API/Presentation/WebAPI/UserOperationCaseStudy.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserOperationCaseStudy.WebAPI.Controllers
+{
+    public class BaseController:ControllerBase
+    {
+        protected readonly IMediator Mediator;
+        public BaseController(IMediator mediator)
+        {
+            Mediator = mediator;
+        }
+    }
+}
---
+++ src/API/Presentation/WebAPI/UserOperationCaseStudy.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UserOperationCaseStudy.Application.Features.Users.Commands.CreateUser;
+using UserOperationCaseStudy.Application.Features.Users.Commands.DeleteUser;
+using UserOperationCaseStudy.Application.Features.Users.Commands.UpdateUser;
+using UserOperationCaseStudy.Application.Features.Users.Queries.GetUserById;
+using UserOperationCaseStudy.Application.Features.Users.Queries.GetUsers;
+
+namespace UserOperationCaseStudy.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : BaseController
+    {
+        public UsersController(IMediator mediator) : base(mediator)
+        {
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] CreateUserRequest request)
+        {
+            return Ok(await Mediator.Send(request));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+
+            return Ok(await Mediator.Send(new DeleteUserRequest { Id = id }));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody] UpdateUserRequest request)
+        {
+            return Ok(await Mediator.Send(request));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var request = new GetAllUsersRequest();
+            return Ok(await Mediator.Send(request));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await Mediator.Send(new GetUserRequest { Id = id }));
+        }
+    }
+}
