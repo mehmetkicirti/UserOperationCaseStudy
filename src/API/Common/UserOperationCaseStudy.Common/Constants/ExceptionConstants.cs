ff9cedfabd24f0f26c59cc8a4723c0e69c1ed2d3
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:12:39 +0300

    Constant messages has been added.

---
+++ src/API/Common/UserOperationCaseStudy.Common/Constants/ExceptionConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperationCaseStudy.Common.Constants
+{
+    public class ExceptionConstants
+    {
+        public const string GUID_IS_NULL = "Related object is null.";
+        public const string VALIDATION_TYPE_ERROR = "It is not assignable for IValidator type class.";
+        public const string UNAUTHORIZE_ERROR = "UnAuthorized Error. This request can not be accessible for you.";
+        public const string USER_EXIST_ERROR = "User already exists. Please try another email address.";
+        public const string USER_NOT_EXIST_ERROR = "User not exists. Please try another email address.";
+        public const string RECORD_NOT_SAVED_ERROR = "While saving your record, something went wrong. Please try again later.";
+    }
+}
---
+++ src/API/Common/UserOperationCaseStudy.Common/Constants/ResponseConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperationCaseStudy.Common.Constants
+{
+    public class ResponseConstants
+    {
+        public const string REGISTER_SUCCESSFULLY = "User is registered successfully.";
+        public const string CREATE_ENTITY_FAILED = "While adding entity, It occurred an error.";
+        public const string GUID_IS_NOT_NULL = "GUID should not be null.";
+        public const string ENTITY_NOT_EXIST = "Searched entity not found. Maybe you are using wrong object id";
+        public const string DELETE_ENTITY_SUCCESFULLY = "Deleted entity value successfully.";
+        public const string DELETE_ENTITY_FAILED = "While deleting entity, It occurred an error.";
+        public const string UPDATE_ENTITY_SUCCESFULLY = "Updated entity value successfully.";
+        public const string UPDATE_ENTITY_FAILED = "While updating entity, It occurred an error.";
+    }
+}
