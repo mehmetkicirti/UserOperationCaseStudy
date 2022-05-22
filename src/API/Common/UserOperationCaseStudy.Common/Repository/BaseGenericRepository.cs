a4d973686063cdcbf2f387ca9c85209d69e70540
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:15:05 +0300

    Repository Pattern usage.

---
+++ src/API/Common/UserOperationCaseStudy.Common/Repository/BaseGenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperationCaseStudy.Common.Domain.Abstract;
+
+namespace UserOperationCaseStudy.Common.Repository
+{
+    public class BaseGenericRepository<T, TContext> : IGenericRepository<T>
+        where T : BaseEntity
+        where TContext: DbContext, new()
+    {
+        protected DbSet<T> Entity { get; }
+        private TContext Context { get; }
+        public BaseGenericRepository(TContext context)
+        {
+            Context = context;
+            Entity = context.Set<T>();
+        }
+
+        #region Sync Ops.
+        public IQueryable<T> AsQueryable() => Entity.AsQueryable();
+
+        public int Delete(T entity)
+        {
+            Context.Remove(entity);
+            return Context.SaveChanges();
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> predicate, bool isTracking = true) => GetQuery(isTracking).Where(predicate);
+        
+        public int Update(T entity)
+        {
+            Context.Update(entity);
+            return Context.SaveChanges();
+        }
+        #endregion
+
+        #region Async Ops.
+        public async Task<IList<T>> GetAllAsync(bool isTracking = true) => await GetQuery(isTracking).ToListAsync();
+        public async Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate, bool isTracking = true) => await GetQuery(isTracking).Where(predicate).ToListAsync();
+        public async Task<T> GetById(string id, bool isTracking = true) => await GetQuery(isTracking).FirstOrDefaultAsync(x => x.Id == Guid.Parse(id));
+        public async Task<int> AddAsync(T entity)
+        {
+            await Context.AddAsync(entity);
+            return await Context.SaveChangesAsync();
+        }
+        #endregion
+
+        #region Logics
+        private IQueryable<T> GetQuery(bool isTracking)
+        {
+            var query = AsQueryable();
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query;
+        }
+        #endregion
+    }
+}
---
+++ src/API/Common/UserOperationCaseStudy.Common/Repository/IGenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperationCaseStudy.Common.Domain.Abstract;
+
+namespace UserOperationCaseStudy.Common.Repository
+{
+    public interface IGenericRepository<T>
+        where T: BaseEntity
+    {
+        #region Async Ops.
+        Task<int> AddAsync(T entity);
+        Task<IList<T>> GetAllAsync(bool isTracking = true);
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate, bool isTracking = true);
+        Task<T> GetById(string id, bool isTracking = true);
+        #endregion
+
+
+        #region Sync Ops.
+        int Update(T entity);
+        int Delete(T entity);
+        IQueryable<T> AsQueryable();
+        IQueryable<T> Get(Expression<Func<T, bool>> predicate, bool isTracking = true);
+        #endregion
+    }
+}
