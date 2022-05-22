27265f28410415f4847071d01bf1b8abc03225a3
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:14:42 +0300

    Global handling mechanicsm has been injected to get usable error messages on response.

---
+++ src/API/Common/UserOperationCaseStudy.Common/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperationCaseStudy.Common.Middlewares;
+
+namespace UserOperationCaseStudy.Common.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigurateCustomExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
---
+++ src/API/Common/UserOperationCaseStudy.Common/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,58 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperationCaseStudy.Common.Exceptions;
+using UserOperationCaseStudy.Common.Wrappers.Concretes;
+
+namespace UserOperationCaseStudy.Common.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private const string ContentType = "application/json";
+        private const string ErrorMessage = "Internal Server Error";
+
+        private RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = ContentType;
+
+            string errorMessage = ErrorMessage;
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+
+            if (ex is DatabaseException || ex is ValidationException)
+            {
+                errorMessage = ex.Message;
+                statusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+            context.Response.StatusCode = statusCode;
+
+            return context.Response.WriteAsync(
+                new ErrorResponse(errorMessage, statusCode).ToString()
+                );
+        }
+    }
+}
