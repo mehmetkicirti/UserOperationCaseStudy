b13f1c7948b58baf0fba45503d51c145987ed0e2
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:12:19 +0300

    Validation Aspect has been added used with Fluent Validation

---
+++ src/API/Common/UserOperationCaseStudy.Common/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -0,0 +1,35 @@
+﻿using Castle.DynamicProxy;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperationCaseStudy.Common.Constants;
+using UserOperationCaseStudy.Common.CrossCuttingConcerns.Validation;
+using UserOperationCaseStudy.Common.Helpers.Interceptors;
+
+namespace UserOperationCaseStudy.Common.Aspects.Autofac.Validation
+{
+    public class ValidationAspect : MethodInterception
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+                throw new Exception(ExceptionConstants.VALIDATION_TYPE_ERROR);
+            _validatorType = validatorType;
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.BaseType?.GetGenericArguments()[0]; // AbstractValidator<User> => It takes User
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+
+            foreach (var entity in entities)
+            {
+                ValidationTool.ValidateAsync(validator, entity).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ src/API/Common/UserOperationCaseStudy.Common/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperationCaseStudy.Common.CrossCuttingConcerns.Validation
+{
+    public class ValidationTool
+    {
+        public async static Task ValidateAsync(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = await validator.ValidateAsync(context);
+
+            if (!result.IsValid)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}
