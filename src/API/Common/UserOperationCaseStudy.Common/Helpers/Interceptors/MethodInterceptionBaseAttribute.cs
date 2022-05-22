d7dabf7d168dd9d4eeaaed2dbb609bc4ebc0929b
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:20:42 +0300

    Interceptors added powered by Autofac

---
+++ src/API/Common/UserOperationCaseStudy.Common/Helpers/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserOperationCaseStudy.Common.Helpers.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class MethodInterceptionBaseAttribute: Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+        public virtual void Intercept(IInvocation invocation)
+        {
+        }
+    }
+}
