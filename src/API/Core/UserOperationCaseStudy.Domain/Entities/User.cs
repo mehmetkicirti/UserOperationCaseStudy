c0bcf6db7045c75dfbf51c3027ba0a4fb1e23537
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-22 04:13:05 +0300

    Domain layer has been finished.

---
+++ src/API/Core/UserOperationCaseStudy.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserOperationCaseStudy.Common.Domain.Abstract;
+
+namespace UserOperationCaseStudy.Domain.Entities
+{
+    public class User: BaseEntity
+    {
+        public string ImagePath { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
